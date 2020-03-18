7a2965b6867b850a6eb9fa2c72c3e0336424cf8f
Jenna-yuan <61236210+Jenna-yuan@users.noreply.github.com>
2020-03-18 09:35:50 +0800

    Add files via upload

---
+++ admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO `admin` (`id`, `name`, `password`, `staus`) VALUES (1, '王嫄', '123456', 0);
+INSERT INTO `admin` (`id`, `name`, `password`, `staus`) VALUES (2, 'root', '123456', 1);
+INSERT INTO `admin` (`id`, `name`, `password`, `staus`) VALUES (3, '祁甜', '123456', 1);
